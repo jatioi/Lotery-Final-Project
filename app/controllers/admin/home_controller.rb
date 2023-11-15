da1f47d6420cf061eccc09d6939e8cad68f371bd
LAPTOP-ND1R9M6U\User <jsuzibloguen@gmail.com>
2023-11-15 12:58:38 +0800

    [Feature 9] Add: Admin User list

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,6 +1,8 @@
 class Admin::HomeController < ApplicationController
   before_action :authenticate_admin_user!
 
-  def index; end
+  def index
+ @client_users = User.client.includes(:children).with_children_total_deposit
+  end
 end
 
