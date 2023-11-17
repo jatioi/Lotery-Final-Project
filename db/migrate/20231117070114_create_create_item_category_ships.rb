1a3a59a478a351f353f38484e91e702f7e395ca9
Jatioi <Jsuzibloguen@gmail.com>
2023-11-17 15:32:34 +0800

    [Feature 12] Add: Many to many with Item & Category

---
+++ db/migrate/20231117070114_create_create_item_category_ships.rb
@@ -0,0 +1,10 @@
+class CreateCreateItemCategoryShips < ActiveRecord::Migration[7.0]
+  def change
+    create_table :item_category_ships do |t|
+      t.references :item
+      t.references :category
+
+      t.timestamps
+    end
+  end
+end
